da430cbe80ac1fb745c04629ccf18a91116a1cde
PainIncoming <galko3.v@gmail.com>
2018-09-14 13:58:47 +0300

    Write a method makeExchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+  let chars  = ['H', 'Q', 'D', 'N', 'P'],
+      values = [50, 25, 10, 5, 1],
+      result = {};
+
+  if (currency > 0 && currency < 10000) {
+    values.reduce((remained, item, i) => {
+      if(item <= remained) {
+        result[chars[i]] = parseInt(remained / item);
+
+        return remained - result[chars[i]] * item;
+      }
+
+      return remained;
+    }, currency);
+  } else if (currency > 9999) {
+    result.error = `You are rich, my friend! We don't have so much coins for exchange`;
+  }
+
+  return result;
+};
